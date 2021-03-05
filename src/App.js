8e0dd85f53d432a41959e7e2415bd5703e11af62
Ana <anaurrig@gmail.com>
2021-03-05 08:39:35 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+const elements = [
+  {
+    image: "/images/icon1.png",
+    title: "Declarative",
+    description: "React makes it painless to create interactive UIS."
+  },
+  {
+    image: "/images/icon2.png",
+    title: "Components",
+    description: "Build encapsulated components that manage their state"
+  },
+  {
+    image: "/images/icon3.png",
+    title: "Single-Way",
+    description: "A set of immutable values are passed to the components"
+  },
+  {
+    image: "/images/icon4.png",
+    title: "JSX",
+    description: "Statically typed, designed to run on modern browsers."
+  }
+]
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav className="App-navbar">
+        <div className="container">
+          <img className="navbar-brand" src="/images/ironhack-logo.svg" alt="logo" />
+          <img src="/images/menu-top.svg" alt="logo" />
+        </div>
+      </nav>
+      <header className="App-header container">
+        <h1 classNmae="text-lg fw-bold mb-2">Say hello to ReactJS</h1>
+        <p>You will learn how to use the most popular fronted library, and become a super Ninja developer.</p>
+        <button className="btn btn-outline-success">Awesome!</button>
       </header>
+      <section>
+      <div className="card-group row">
+        {elements.map(element =>
+            <div className="card col-4">
+              <img className="card-img-top" size="20%" src={element.image} alt="Card image cap" />
+              <h6 className="App-h6">{element.title}</h6>
+              <p>{element.description}</p>
+            </div>
+        )}
+          </div>
+      </section>
     </div>
   );
 }
